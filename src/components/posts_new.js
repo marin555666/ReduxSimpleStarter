90fba48076e062cea0343529c81a99301df06d9e
mkrmek <mkrmek@iolap.com>
2018-10-23 15:56:39 +0200

    Finished section 6

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createPost } from '../actions'
+
 class PostsNew extends Component {
   renderField(field) {
+    const { meta : { touched, error } } = field
+    const className=`form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input 
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
+      // field.meta.touched ? field.meta.error : '' 
+      // if nothing is typed in the field and user clicks
+      // outside of it the error message appears
     )
   }
 
   onSubmit(values) {
-    console.log(values)
+    this.props.createPost(values, () => {
+      this.props.history.push('/')
+    })
   }
 
   render() {
     const { handleSubmit } = this.props
-    
+
     return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field 
@@ -40,12 +53,8 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-        <Link className="btn btn-primary" to="/">
-            Cancel
-        </Link>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link className="btn btn-danger" to="/">Cancel</Link>
       </form>
     )
   }
@@ -70,4 +79,6 @@ function validate(values) {
 export default reduxForm({
   validate, // validate: validate
   form: 'PostsNewForm'
-})(PostsNew)
+})(
+  connect(null, { createPost })(PostsNew)
+)
